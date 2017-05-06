8dafd1c2ad42286c2b31ffffaff7739da9d754f4
Carlos <carlosbenitez1995@gmail.com>
2017-05-06 11:06:30 -0500

    Aplicacion calcular solo suma terminada

--- app/src/main/java/com/example/carlos/myapplication/MainActivity.java
+++ app/src/main/java/com/example/carlos/myapplication/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.carlos.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText cajanumerouno, cajanumerodos;
+    private TextView cajaresultado;
+    int n1,n2,suma;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cajanumerouno = (EditText)findViewById(R.id.editText3);
+        cajanumerodos = (EditText)findViewById(R.id.editText4);
+        cajaresultado = (TextView)findViewById(R.id.txtresultado);
+
     }
+
+    public void calcular(View v) {
+
+        double n1, n2, suma;
+
+        if (validar()) {
+            n1 = Double.parseDouble(cajanumerouno.getText().toString());
+            n2 = Double.parseDouble(cajanumerodos.getText().toString());
+            suma = n1 + n2;
+            cajaresultado.setText("" + suma);
+        }
+    }
+
+    private boolean validar() {
+        if (cajanumerouno.getText().toString().isEmpty()){
+            cajanumerouno.setError(this.getResources().getString(R.string.error_numero_uno));
+            cajanumerouno.requestFocus();
+            return false;
+        } else if (cajanumerodos.getText().toString().isEmpty()){
+            cajanumerodos.setError(this.getResources().getString(R.string.error_numero_dos));
+            cajanumerodos.requestFocus();
+            return false;
+        }return true;
+    }
+
+    public void borrar(View v){
+        cajanumerouno.setText("");
+        cajanumerodos.setText("");
+        cajaresultado.setText("");
+        cajanumerouno.requestFocus();
+    }
+
+
 }
